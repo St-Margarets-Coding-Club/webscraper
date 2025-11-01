169e96430d63ab83e6f267c495255271b7642459
Bcninja <haydenlo01027@gmail.com>
2025-11-01 20:26:57 +0800

    Added animation and enter support

--- Web/script.js
+++ Web/script.js
@@ -1,7 +1,21 @@
+const InputBox = document.querySelector(".InputBox");
+
 function MoveDownInputBox() {
-        const InputBox = document.querySelector(".InputBox");
         InputBox.classList.add("Move_Down");
 
-        const Instruction = document.getElementById('Instruction');
+        const Instruction = document.getElementById("Instruction");
         Instruction.remove();
+
+        const Input = document.getElementById("Input");
+        Input.placeholder = "Type your message";
+        Input.value = '';
+
+        CreateChatBox();
 }
+
+InputBox.addEventListener("keydown", (e) => {
+        if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                MoveDownInputBox();
+        }
+});
