5e5b811274defc304db56b749143d16dc2071aaf
Bcninja <haydenlo01027@gmail.com>
2025-11-09 14:15:11 +0800

    UI patches

--- Web/script.js
+++ Web/script.js
@@ -1,21 +1,24 @@
 const InputBox = document.querySelector(".InputBox");
 
 function MoveDownInputBox() {
-        InputBox.classList.add("Move_Down");
+        if (Input.value != '') {
+                InputBox.classList.add("Move_Down");
 
-        const Instruction = document.getElementById("Instruction");
-        Instruction.remove();
+                const Instruction =
+                        document.getElementById("Instruction");
+                Instruction.remove();
 
-        const Input = document.getElementById("Input");
-        Input.placeholder = "Type your message";
-        Input.value = '';
-
-        CreateChatBox();
+                const Input = document.getElementById("Input");
+                Input.placeholder = "Type your message";
+                Input.value = "";
+        }
 }
 
 InputBox.addEventListener("keydown", (e) => {
-        if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                MoveDownInputBox();
+        if (Input.value != '') {
+                if (e.key === "Enter" && !e.shiftKey) {
+                        e.preventDefault();
+                        MoveDownInputBox();
+                }
         }
-});
+})
